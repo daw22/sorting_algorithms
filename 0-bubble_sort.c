136223235fee4cfba6036d89a69042e1a619bdf8
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-17 08:25:44 +0000

    Add check to break if the array is sorted (no swaps in an iteration)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,12 +8,13 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, tmp;
+	size_t i, j, tmp, swapped;
 
 	if (size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,8 +22,11 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		if (!swapped)
+			break;
 	}
 }
