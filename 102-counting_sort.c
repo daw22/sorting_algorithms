db1fe22c6a03d472eda584ecc2d78116f2c9237b
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-20 13:16:37 +0000

    Add check for NULL and correct answers

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,29 +11,32 @@ void counting_sort(int *array, size_t size)
 {
 	int *count_arr, max = 0, i, j = 0;
 
-	for (i = 0; (size_t) i < size; i++)
+	if (array)
 	{
-		if (array[i] > max)
-			max = array[i];
-	}
-	count_arr = malloc(sizeof(int) * (max + 1));
-	if (count_arr)
-	{
-		for (i = 0; i <= max; i++)
-			count_arr[i] = 0;
 		for (i = 0; (size_t) i < size; i++)
-			count_arr[array[i]] += 1;
-		for (i = 1; i <= max; i++)
-			count_arr[i] += count_arr[i - 1];
-		print_array(count_arr, max + 1);
-		for (i = 0; i <= max; i++)
 		{
-			while (j < count_arr[i])
-			{
-				array[j] = i;
-				j++;
-			}
+			if (array[i] > max)
+				max = array[i];
 		}
-		free(count_arr);
+		count_arr = malloc(sizeof(int) * (max + 1));
+		if (count_arr)
+		{
+			for (i = 0; i <= max; i++)
+				count_arr[i] = 0;
+			for (i = 0; (size_t) i < size; i++)
+				count_arr[array[i]] += 1;
+			for (i = 1; i <= max; i++)
+				count_arr[i] += count_arr[i - 1];
+			print_array(count_arr, max + 1);
+			for (i = 0; i <= max; i++)
+			{
+				while (j < count_arr[i])
+				{
+					array[j] = i;
+					j++;
+				}
+			}
+			free(count_arr);
+		}
 	}
 }
